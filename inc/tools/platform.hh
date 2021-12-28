4e8cc6ec406225c8614c29f34d263b5483249e17
Supdrewin <supdrewin@outlook.com>
2021-12-28 19:51:42 +0800

    Fix for macOS

--- inc/tools/platform.hh
+++ inc/tools/platform.hh
@@ -2,14 +2,14 @@
 
 #include <cstdlib>
 
-#if defined(__unix__)
-#define CLEAR() system("clear")
-#define PAUSE() system("bash -c 'read -n 1'")
-#include <unistd.h>
-#define SLEEP(sec) sleep(sec)
-#elif defined(_WIN32)
+#if defined(_WIN32)
 #define CLEAR() system("cls")
 #define PAUSE() system("pause")
 #include <windows.h>
 #define SLEEP(sec) Sleep(sec * 1000)
-#endif // defined(__unix__)
+#else // !defined(_WIN32)
+#define CLEAR() system("clear")
+#define PAUSE() system("bash -c 'read -n 1'")
+#include <unistd.h>
+#define SLEEP(sec) sleep(sec)
+#endif // defined(_WIN32)
