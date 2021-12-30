5a60412f00e06550993df2f3a6a0f7b284544c0d
Supdrewin <supdrewin@outlook.com>
2021-12-30 11:00:42 +0800

    fix logo-gen and update .gitignore

--- tools/logo-gen.sh
+++ tools/logo-gen.sh
@@ -9,12 +9,13 @@ tmp=${src//
 /\\x0a}
 tmp=${tmp// /\\x20}
 tmp=${tmp//#/\\x1b\\x5b\\x34\\x33\\x6d\\x20\\x1b\\x5b\\x30\\x6d}
+tmp=${tmp//\\x1b\\x5b\\x30\\x6d\\x1b\\x5b\\x34\\x33\\x6d/}
 
 echo "#pragma once
 
 #if defined(__has_logo)
 
-static const char *logo = \"$tmp\"
+static const char *logo = \"$tmp\\x0a\";
 
 #endif // defined(__has_logo)" >"$dist"
 clang-format -i "$dist"
