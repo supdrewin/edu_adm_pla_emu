8e1bf0ee2b5388b5cca8bf1a5d539a4b4108cb26
Supdrewin <supdrewin@outlook.com>
2021-12-30 10:49:39 +0800

    Update logo-gen

---
+++ tools/logo-gen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+[[ $1 ]] || exit 1
+
+dist=${1%%.*}.hh
+src=$(cat "$1")
+
+tmp=${src//
+/\\x0a}
+tmp=${tmp// /\\x20}
+tmp=${tmp//#/\\x1b\\x5b\\x34\\x33\\x6d\\x20\\x1b\\x5b\\x30\\x6d}
+
+echo "#pragma once
+
+#if defined(__has_logo)
+
+static const char *logo = \"$tmp\"
+
+#endif // defined(__has_logo)" >"$dist"
+clang-format -i "$dist"
